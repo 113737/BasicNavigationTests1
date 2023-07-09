0d484708fd1e58c4dd7c752c587ffbb72c641c45
Kubra Taskin <kvc.taskin@gmail.com>
2023-07-09 16:43:41 +0300

    create StringUtilities class

---
+++ src/test/java/com/cbt/utilities/StringUtilities.java
@@ -0,0 +1,30 @@
+package com.cbt.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StringUtilities {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://google.com");
+
+        WebElement googleSearchBox= driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("safari"+ Keys.ENTER);
+
+        String expected = "safari";
+        String actual= driver.getTitle();
+        if (actual.equals(expected)) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+        driver.close();
+
+    }
+}
